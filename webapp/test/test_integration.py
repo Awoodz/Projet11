f91fc171fc8fa7d67a19fdb8cf2b76a598e738ec
Awoodz <a.bois77@gmail.com>
2020-10-14 14:58:31 +0200

    new tests for recent errors

--- webapp/test/test_integration.py
+++ webapp/test/test_integration.py
@@ -27,7 +27,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_login(self):
+    def test_login_redirect_to_index(self):
         self.selenium.get("%s%s" % (self.live_server_url, "/accounts/signup/"))
         username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys("testuser")
@@ -51,3 +51,28 @@ class MySeleniumTests(StaticLiveServerTestCase):
             EC.presence_of_element_located((By.CLASS_NAME, "pb_userform"))
         )
         assert self.selenium.current_url == self.live_server_url + "/"
+
+    def test_login_does_not_redirect_to_login(self):
+        self.selenium.get("%s%s" % (self.live_server_url, "/accounts/signup/"))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys("testuser")
+        email_input = self.selenium.find_element_by_name("email")
+        email_input.send_keys("testuser@mail.com")
+        password_input = self.selenium.find_element_by_name("password1")
+        password_input.send_keys("testpass1")
+        password_input = self.selenium.find_element_by_name("password2")
+        password_input.send_keys("testpass1")
+        self.selenium.find_element_by_xpath('//button[@value="signup"]').click()
+
+        WebDriverWait(self.selenium, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "login_form"))
+        )
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys("testuser")
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys("testpass1")
+        self.selenium.find_element_by_xpath('//button[@value="login"]').click()
+        WebDriverWait(self.selenium, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "pb_userform"))
+        )
+        assert self.selenium.current_url != self.live_server_url + "/login"
--- webapp/test/test_search_help_view.py
+++ webapp/test/test_search_help_view.py
@@ -1,8 +1,23 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
 
 
 class SearchHelpPageTestCase(TestCase):
     def test_search_help_returns_200(self):
-        response = self.client.get(reverse("search_help"), {"query": "fa",})
+        response = self.client.get(
+            reverse("search_help"),
+            {
+                "query": "fa",
+            },
+        )
         self.assertEqual(response.status_code, 200)
+
+    def test_search_help_page_name_is_not_search_details(self):
+        with self.assertRaises(NoReverseMatch):
+            self.client.get(
+                reverse("search_details"),
+                {
+                    "query": "fa",
+                },
+            )
